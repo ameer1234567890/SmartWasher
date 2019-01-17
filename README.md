8eda39bb4951923b0ac287ebf2238c9d34af66c5
Ameer Dawood <ameer1234567890@gmail.com>
2019-01-17 20:09:58 +0500

    Washer done...

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 ## SmartWasher
 A smart washer using Arduino
 
-* This is a WIP (Work in progress)
+**This is a WIP (Work in progress)**
+
+#### How it works
+* Arduino waits for vibrations on the sensor, called __ticks__.
+* When a tick is detected, a variable named `probableStart` is incremented.
+* Simultaneously, a counter is started, counting down to 45 seconds.
+* When `probableStart` reaches 5 (5 tickds), the washer is deemed to have started.
+* If the aforementioned countdown finishes before reaching 5 ticks, `probableStart` is reset to zero.
--- SmartWasher.ino
+++ SmartWasher.ino
@@ -2,21 +2,22 @@
 
 #define BUZZER_PIN 12
 #define SENSOR_PIN A0
+#define LED_PIN 13
 int sensorValue;
 int minSense = 950;
 int delayBetweenChecks = 5000; // 5 seconds
 int numTicks = 5;
 int probableStart = 0;
-int probableEnd = 0;
+int probableFinish = 0;
 unsigned long timeTillDetection = 45000; // 45 seconds
 unsigned long startMillis;
 unsigned long currentMillis;
-bool washerStarted = false;
-bool washerFinished = false;
+bool washing = false;
 
 void setup() {
   pinMode(SENSOR_PIN, INPUT);
   pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
   Serial.begin(115200);
   Serial.println("Start up");
 }
@@ -24,9 +25,10 @@ void setup() {
 void loop() {
   sensorValue = analogRead(SENSOR_PIN);
   //Serial.println(sensorValue);
-  if (!washerStarted) {
+  if (!washing) {
+    digitalWrite(LED_PIN, 0);
     if (sensorValue < minSense) {
-      Serial.print("sensorValue: ");
+      Serial.print("Not Started - sensorValue: ");
       Serial.println(sensorValue);
       if (probableStart == 0) {
         startMillis = millis();
@@ -44,13 +46,34 @@ void loop() {
       delay(delayBetweenChecks);
     }
     if (probableStart >= numTicks) {
-      washerStarted = true;
+      washing = true;
       Serial.print(probableStart);
       Serial.println(" ticks registered! Washer start deemed!");
     }
-  } else if (washerStarted) {
-    Serial.println("Washer already started!");
-    // Finish detection algorithm goes here
-    delay(1000);
+  } else {
+    digitalWrite(LED_PIN, 1);
+    if (sensorValue > minSense) {
+      Serial.print("Started - sensorValue: ");
+      Serial.println(sensorValue);
+      if (probableFinish == 0) {
+        startMillis = millis();
+      }
+      probableFinish++;
+      currentMillis = millis();
+      if (currentMillis > (startMillis + timeTillDetection)) {
+        probableFinish = 0;
+      }
+      tone(BUZZER_PIN, 1000);
+      delay(50);
+      noTone(BUZZER_PIN);
+      Serial.print("probableFinish: ");
+      Serial.println(probableFinish);
+      delay(delayBetweenChecks);
+    }
+    if (probableFinish >= numTicks) {
+      washing = false;
+      Serial.print(probableFinish);
+      Serial.println(" ticks registered! Washer start deemed!");
+    }
   }
 }
