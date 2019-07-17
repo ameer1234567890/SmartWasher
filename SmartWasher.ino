358d25a1022050cb5f01b1360300cf72ae53bd2e
Ameer Dawood <ameer1234567890@gmail.com>
2019-07-17 13:10:59 +0500

    Notification and logging

--- SmartWasher.ino
+++ SmartWasher.ino
@@ -1,17 +1,22 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <ESP8266WebServer.h>
+#include <ArduinoOTA.h>
+#include <time.h>
 #include "Secrets.h"
 
-/*
-Secrets.h file should contain data as below:
+/* Secrets.h file should contain data as below: */
+#ifndef WIFI_SSID
 #define WIFI_SSID "xxxxxxxxxx"
 #define WIFI_PASSWORD "xxxxxxxxxx"
 #define IFTTT_URL "http://maker.ifttt.com/trigger/washer_finished/with/key/xxxxxxxxxxxxxxxxxxxxxx"
-*/
+#endif
 
 #define SENSOR_PIN D3
 #define BUZZER_PIN D4
 #define LED_PIN D5
+#define OTA_HOSTNAME "SmartWasher"
+#define SERVER_PORT 80
 
 long lastTickTime;
 //long lastTime;
@@ -21,16 +26,42 @@ int tickCount = 0;
 int delayBetweenTicks = 1000; // 1 second
 int resetInterval = 10000; // 10 seconds
 int numTicksRequired = 10;
+const int TIMEZONE = 5;
+const int DST = 0;
+String logTime;
+String logMsg;
 WiFiClient wClient;
+ESP8266WebServer server(SERVER_PORT);
 
 
 void setup() {
+  log("I/system: startup");
   pinMode(SENSOR_PIN, INPUT);
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
-  Serial.begin(115200);  
-  Serial.println("\nStartup");
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.begin(115200);
   setupWifi();
+  setupTime();
+  server.on("/", []() {
+    server.send(200, "text/html", "\
+      <a href=\"/log\">/log</a><br>\
+      <a href=\"/reboot\">/reboot</a><br>\
+    ");
+    log("I/server: served / to " + server.client().remoteIP().toString());
+  });
+  server.on("/log", []() {
+    server.send(200, "text/plain", logMsg);
+    log("I/server: served /log to " + server.client().remoteIP().toString());
+  });
+  server.on("/reboot", []() {
+    server.send(200, "text/plain", "rebooting");
+    delay(1000);
+    ESP.restart();
+  });
+  server.begin();
+  ArduinoOTA.setHostname(OTA_HOSTNAME);
+  ArduinoOTA.begin();
 }
 
 
@@ -40,35 +71,32 @@ void loop() {
     if (sensorValue == LOW && millis() > (lastTickTime + delayBetweenTicks)) {
       tickCount++;
       lastTickTime = millis();
-      Serial.print("Vibration detected. tickCount: ");
-      Serial.println(tickCount);
+      log("I/ticker: vibration detected. tickCount => " + String(tickCount));
     }
     if (tickCount > numTicksRequired) {
       washing = true;
-      Serial.println("Washing started!");
+      log("I/ticker: washing started!");
     }
   } else {
     if (sensorValue == LOW) {
       if (millis() > (lastTickTime + delayBetweenTicks)) {
         lastTickTime = millis();
         tickCount++;
-        Serial.print("Vibration detected! tickCount: ");
-        Serial.println(tickCount);
+        log("I/ticker: vibration detected. tickCount => " + String(tickCount));
       }
       if (tickCount > numTicksRequired) {
         tickCount = numTicksRequired;
-        Serial.print("Ticker going too high! Reset! tickCount: ");
-        Serial.println(tickCount);
+        log("I/ticker: ticker going too high! reset! tickCount => " + String(tickCount));
       }
     }
     if (tickCount == 0) {
       washing = false;
-      Serial.println("Washing finished!");
+      log("I/ticker: washing finished!");
       if(WiFi.status() == WL_CONNECTED) {
-        Serial.println("Wifi was connected!");
+        log("I/ticker: wifi was connected!");
         notify();
       } else {
-        Serial.println("Wifi was disconnected!");
+        log("I/ticker: wifi was disconnected!");
         setupWifi();
         notify();
       }
@@ -78,28 +106,29 @@ void loop() {
   if (!washing && (millis() - lastTickTime) > resetInterval) {
     if (tickCount > 0) {
       tickCount = 0;
-      Serial.print("Ticker reset due to inactivity! tickCount: ");
-      Serial.println(tickCount);
+      log("I/ticker: ticker reset due to inactivity! tickCount => " + String(tickCount));
     }
   }
 
   if (washing && (millis() - lastTickTime) > resetInterval) {
     tickCount--;
-    Serial.print("Ticker reduced due to inactivity! tickCount: ");
-    Serial.println(tickCount);
+    log("I/ticker: ticker reduced due to inactivity! tickCount => " + String(tickCount));
     lastTickTime = millis();
   }
+
+  server.handleClient();
+  ArduinoOTA.handle();
 }
 
 
 void notify() {
   if (postToIfttt()) {
     digitalWrite(LED_PIN, HIGH);
-    Serial.println("Done!");
+    log("I/notify: notified via IFTTT!");
     delay(2000);
     digitalWrite(LED_PIN, LOW);
   } else {
-    Serial.println("IFTTT did not work!");
+    log("E/notify: IFTTT did not work!");
     digitalWrite(LED_PIN, HIGH);
     delay(200);
     digitalWrite(LED_PIN, LOW);
@@ -122,10 +151,10 @@ bool postToIfttt() {
   httpCode = http.GET();
   http.end();
   if (httpCode == 200) {
-    Serial.println(httpCode);
+    log("I/notify: IFTTT response => " + String(httpCode));
     return true;
   } else {
-    Serial.println(httpCode);
+    log("E/notify: IFTTT response => " + String(httpCode));
     return false;
   }
 }
@@ -134,23 +163,39 @@ bool postToIfttt() {
 void setupWifi() {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
     digitalWrite(LED_PIN, HIGH);
-    delay(50);
+    delay(10);
     digitalWrite(LED_PIN, LOW);
-    delay(50);
-    digitalWrite(LED_PIN, HIGH);
-    delay(50);
-    digitalWrite(LED_PIN, LOW);
-    delay(200);
+    delay(10);
   }
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  log("I/wifi  : wifi connected");  
+  log("I/wifi  : IP address => " + WiFi.localIP().toString());
   delay(700);
   digitalWrite(LED_PIN, HIGH);
   delay(1000);
   digitalWrite(LED_PIN, LOW);
+}
+
+
+void log(String msg) {
+  time_t now = time(0);
+  logTime = ctime(&now);
+  logTime.trim();
+  logMsg = logMsg + "[" + logTime + "] ";
+  logMsg = logMsg + msg + "\n";
+  Serial.println(msg);
+}
+
+
+void setupTime() {
+  configTime(TIMEZONE * 3600, DST, "pool.ntp.org", "time.nist.gov");
+  log("I/time  : waiting for time");
+  while (!time(nullptr)) {
+    delay(100);
+  }
+  delay(100);
+  time_t now = time(0);
+  logTime = ctime(&now);
+  logTime.trim();
+  log("I/time  : time obtained via NTP => " + logTime);
 }
